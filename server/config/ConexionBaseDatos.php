d876eaa2118007aa6cb8026d809ed468e0078fe9
vessel9 <jjaramilloa@institutobenitojuarez.edu.ec>
2017-12-06 13:05:57 -0500

    host

--- server/config/ConexionBaseDatos.php
+++ server/config/ConexionBaseDatos.php
@@ -6,6 +6,8 @@ class ConexionBaseDatos {
         $array = array();
         $array[] = new DatosConexion("pruebasITSY","172.16.0.9","ignug","prueba","12345678");
         $array[] = new DatosConexion("local","localhost","ignug","prueba","12345678");
+        $array[] = new DatosConexion("local","sql313.byethost15.com","b15_21140729","b15_21140729_MusicalQuality","12345678");
+        
         return $array;
     }
 }
