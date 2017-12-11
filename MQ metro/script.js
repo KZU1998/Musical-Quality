b6366ae1910085ac302aec1a2089c4e4ad773961
vessel9 <jjaramilloa@institutobenitojuarez.edu.ec>
2017-12-11 10:05:46 -0500

    correcion server

--- MQ metro/script.js
+++ MQ metro/script.js
@@ -1,4 +1,4 @@
 var urlWS ="";
 $(document).ready(funtion){
-    urlWS="http://185.27.134.10/db_structure.php?db=b15_21140729_MusicalQuality"
+    urlWS="http://musical-quality.byethost7.com/server/"
 }
--- server/config/ConexionBaseDatos.php
+++ server/config/ConexionBaseDatos.php
@@ -4,8 +4,8 @@ class ConexionBaseDatos {
     private static $array = array();
     public static function DatosConexiones(){
         $array = array();
-        $array[] = new DatosConexion("bytehost","sql313.byethost7.com","b7_21140688","b7_21140688_mq","1998KaZu");
+        $array[] = new DatosConexion("bytehost","sql313.byethost7.com","b7_21140688_mq","b7_21140688","1998KaZu");
         
         return $array;
     }
-}
+}w
