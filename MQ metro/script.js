6c5bd36c6d7059b6b5a3c860786b933c6f857b10
vessel9 <jjaramilloa@institutobenitojuarez.edu.ec>
2017-12-11 20:15:16 -0500

    gurdado

--- MQ metro/script.js
+++ MQ metro/script.js
@@ -2,3 +2,25 @@ var urlWS ="";
 $(document).ready(funtion){
     urlWS="http://musical-quality.byethost7.com/server/"
 }
+function crear(){
+    nombre = document.getElementById("nombre").value;
+    apellido = document.getElementById("apellido").value;
+    edad = document.getElementById("edad").value;
+    email = document.getElementById("email").value;
+    clave = document.getElementById("clave").value;
+    urltorequest = urlWS +"Registro/crear";
+    $.ajax({
+        type: "post",
+        url: urltorequest,
+        data:JSON.stringify({nombre: nombre, apellido: apellido, edad: edad, email: email, clave: clave}),
+        async:false,
+        success:  function (respuesta) {
+            if(respuesta=="false"){
+                alert("Error al crear el registro");
+            }else{
+                alert("Registro creado.");
+            }
+        }
+    });
+    leer();
+}
