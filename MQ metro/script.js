8cb99ac9dadcad4d0f7d0f177ba71e723eaedb8b
vessel9 <jjaramilloa@institutobenitojuarez.edu.ec>
2017-12-16 18:12:36 -0500

    registro prueba 1

--- MQ metro/script.js
+++ MQ metro/script.js
@@ -16,3 +16,26 @@ function leer(){
     });
 
 }
+function crearreg(){
+    nombre = document.getElementById("nombre").value;
+    apellido = document.getElementById("apellido").value;
+    edad = document.getElementById("edad").value;
+    email = document.getElementById("email").value;
+    clave = document.getElementById("clave").value;
+    urltorequest = urlWS +"Registro/leer";
+    $.ajax({
+        type: "post",
+        url: urltorequest,
+        data:JSON.stringify({nombre: nombre, apellido: apellido, edad: edad, email: email, clave: clave}),
+        async:false,
+        success:  function (respuesta) {
+            if(respuesta=="false"){
+                alert("Error al crear el registro");
+            }else{
+                alert("Registro creado.");
+            }
+        }
+    });
+    leer();
+}
+
