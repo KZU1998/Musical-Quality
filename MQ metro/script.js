22f64f8b8a1fda6f12c40eb16ad525b9d826b17d
vessel9 <jjaramilloa@institutobenitojuarez.edu.ec>
2017-12-16 16:12:13 -0500

    cambio

--- MQ metro/script.js
+++ MQ metro/script.js
@@ -1,29 +1,18 @@
 var urlWS = "";
 $(document).ready(function(){
-    urlWS="http://musical-quality.byethost7.com/server"
+    urlWS="http://musical-quality.byethost7.com/server/"
 });
 
 
 function leer(){
-    if(id==0){
-        urltorequest = urlWS +"Generos/leer";
-    }else{
-        urltorequest = urlWS +"Generos/leer";
-    }
-    $.ajax({
+    urltorequest = urlWS +"Generos/leer";
+      $.ajax({
         type: "get",
         url: urltorequest,
         async:true,
         success:  function (respuesta) {
-           toshow = JSON.parse(respuesta);
-           cabeceraTabla = "<table class=\"table table-condensed\"><thead><tr><th>id</th><th>TIPO</th></tr></thead><tbody>";
-           pieTabla = "</tbody></table>";
-           contenidoTabla = "";
-           $(toshow).each(function(key,value){
-                contenidoTabla=contenidoTabla+"<tr><td>"+value.idGeneros+"</td><td>"+value.genero+"</td></tr>";
-           });
-           document.getElementById("respuesta").innerHTML=cabeceraTabla+contenidoTabla+pieTabla;
+          alert("llegaron los datos");
         }
     });
-    limpiar();
+
 }
